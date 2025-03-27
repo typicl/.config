d2f20f7c04259413ecbf109041b3abfe5b3f990d
typicl <typicool@kingdomstudios.io>
2025-03-27 15:12:39 -0600

    feat: Add support for selected word searches

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,3 +5,7 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 		builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pw', builtin.grep_string, {})
+vim.keymap.set('v', '<leader>pw', function()
+		builtin.grep_string({ search = "<C-R>0" });
+end)
